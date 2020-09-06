0d32b49277925b2741bd88d4411e2dd0b7db4819
AggieWB <usmaggie23@gmail.com>
2020-09-06 13:11:43 -0700

    Add rot13() function which uses charCodeAt(), fromCharCode(),
    and regex to use Caesar cipher to decode the string passed in

---
+++ caesars-cipher.js
@@ -0,0 +1,25 @@
+function rot13(str) {
+    let newStr = '';
+    const ASCIICodeA = 65;
+    const ASCIICodeZ = 90;
+    const ROT13Shift = 13;
+    for(let i = 0; i < str.length; i++){
+      if(str[i].match(/[A-Z]/)){
+          const currentStrASCII = str.charCodeAt(i);
+          const shiftedASCII = currentStrASCII + ROT13Shift;
+          let newStrASCII = null;
+          if(str[i].match(/[N-Z]/)){
+              //ASCII code greater than 90 minus 90 plus 64
+              newStrASCII = (shiftedASCII - ASCIICodeZ + ASCIICodeA - 1);
+            } else if (str[i].match(/[A-M]/)){
+                newStrASCII = shiftedASCII;
+            }
+            newStr += String.fromCharCode(newStrASCII);
+        } else {
+            newStr += str[i];
+        }
+    }
+    //Mutate the original string even though the instructions don't state to
+    str = newStr;
+    return str;
+}
